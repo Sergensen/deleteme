92eaaa8b40bb8958fedd8c60ff031bc21a00d089
Sergen Sentürk <Sergen.Sentuerk@ext.experienceone.com>
2022-12-11 20:57:53 +0100

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const users = {
+  michel: { name: "Michel", age: 30 },
+  sergen: { name: "Sergen", age: 25 },
+}
+app.use(bodyparser.json());
+
+app.get("/user", function (req, res) {
+  const { query } = req;
+  res.json(users[query.name] || { error: "User not found" });
+ });
+
+ app.get("/users", function (req, res) {
+  res.json(users);
+ });
+
+ app.post("/user", function (req, res) {
+  const { body } = req;
+  users[body.name] = body;
+  res.json({ success: true });
+ });
+
+ app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+ })
