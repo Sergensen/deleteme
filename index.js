2b73c81f402cfbba95730d30ac03285cfc1ff1e5
Sergen Sentürk <Sergen.Sentuerk@ext.experienceone.com>
2022-12-11 21:15:36 +0100

    cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
+const cors = require('cors');
+
 const users = {
   michel: { name: "Michel", age: 30 },
   sergen: { name: "Sergen", age: 25 },
 }
 app.use(bodyparser.json());
+app.use(cors());
 
 app.get("/user", function (req, res) {
   const { query } = req;
